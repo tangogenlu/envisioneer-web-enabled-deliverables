4a5ee424074ca3ed05a4d7aca3a970e10f4121ef
Геннадий <140153539+tangogenlu@users.noreply.github.com>
2024-01-22 09:57:20 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,57 @@
+        for (int item : data) {
+    }
+
+
+        }
+            System.out.println("Random Number: " + item);
+}
+public class RandomDataGenerator {
+    public static void main(String[] args) {
+        }
+        int[] data = new int[10];
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int i = 0; i < 10; i++) {    const data = [];
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = generateRandomData();
+    }
+    return data;
+}
+
+main();
+
+}
+    });
+    for (let i = 0; i < 10; i++) {
+
+function generateRandomData() {
+    data.forEach(item => {
+        console.log("Random Number: " + item);
+function main() {}
+            System.out.println("Random Number: " + item);
+        }
+    public static void main(String[] args) {
+public class RandomDataGenerator {
+        }
+        for (int i = 0; i < 10; i++) {
+
+        int[] data = new int[10];
+
+            data[i] = (int) (Math.random() * 100) + 1;
+    }
+        for (int item : data) {
+    const data = [];
+}
+
+    for (let i = 0; i < 10; i++) {
+    const data = generateRandomData();
+    return data;
+main();
+
+        console.log("Random Number: " + item);
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+function generateRandomData() {
+}
+    data.forEach(item => {
+    }
